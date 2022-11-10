16ab4e2d455dad60deb76160bfdf886211e6d9ae
Bennet Breier <bennetbreier@gmail.com>
2022-11-10 15:12:12 +0100

    add test for variance

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -2,9 +2,16 @@ import pytest
 
 import meaningful
 
+import numpy as np
 
 def test_mean():
     l = [1.0, 2.0, 3.0]
     result = meaningful.stats.mean(l)
     expected = 2.0
+    assert result == pytest.approx(expected)
+
+def test_variance():
+    l = [1.0, 2.0, 3.0]
+    result = meaningful.stats.variance(l)
+    expected = np.var(l)
     assert result == pytest.approx(expected)
