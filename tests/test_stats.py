f8544636f23f0d71516ea537122803b5bb0f79a1
Simon Brandt <simonbrandt@protonmail.com>
2022-11-10 15:28:42 +0100

    [test_var added to tests]

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -8,3 +8,10 @@ def test_mean():
     result = meaningful.stats.mean(l)
     expected = 2.0
     assert result == pytest.approx(expected)
+
+
+def test_var():
+    l = [1.0, 2.0, 3.0]
+    result = meaningful.stats.var(l)
+    expected = 0.6666666666666666
+    assert result == pytest.approx(expected)
