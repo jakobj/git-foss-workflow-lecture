ca47c0944e92f88bef759ff6cca22fb626485d20
Timo Gierlich <timo@eltima-electronic.de>
2022-11-10 15:31:30 +0100

    Ok, removed the greeting! :/

--- meaningful/stats.py
+++ meaningful/stats.py
@@ -2,8 +2,8 @@ def mean(l):
     n = len(l)
     return 1.0 / n * sum(x for x in l)
 
+
 def variance(l):
     n = len(l)
     mu = mean(l)
-    print('Hello you!')
-    return sum((x - mu)**2 for x in l) / n
+    return sum((x - mu) ** 2 for x in l) / n
